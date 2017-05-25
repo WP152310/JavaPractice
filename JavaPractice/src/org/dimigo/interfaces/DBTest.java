a46855cfd3fc9ad163db363ddb3f9b53aded72c8
a <anonymous@none.kr>
2017-05-25 11:23:26 +0900

    a

---
+++ JavaPractice/src/org/dimigo/interfaces/DBTest.java
@@ -0,0 +1,25 @@
+package org.dimigo.interfaces;
+
+public class DBTest {
+
+	public static void main(String[] args) {
+		IDBManager db = null;
+		System.out.println("<< 변경 전 >>");
+		db = IDBManager.getDBObject("SYBASE");
+		if (db != null)
+			crud(db);
+		System.out.println();
+		System.out.println("<< 변경 후 >>");
+		db = IDBManager.getDBObject("SYBASE");
+		if (db != null)
+			crud(db);
+	}
+
+	private static void crud(IDBManager db) {
+		db.insert();
+		db.search();
+		db.update();
+		db.delete();
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/interfaces/IDBManager.java
@@ -0,0 +1,17 @@
+package org.dimigo.interfaces;
+
+public interface IDBManager {
+	String ORACLE_DATABASE = "ORACLE";
+	String SYBASE_DATABASE = "SYBASE";
+	void insert();
+	void search();
+	void update();
+	void delete();
+	static IDBManager getDBObject(String database){
+		if(database.equals(ORACLE_DATABASE))
+			return new OracleDB();
+		else if(database.equals(SYBASE_DATABASE))
+			return new SybaseDB();
+		else return null;
+	}
+}
